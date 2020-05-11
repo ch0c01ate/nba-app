c1f6dd723aa475fdb1b91200235e377ca9b1e661
Bernadzikovskiy Slavik <vbernadzikovskiy@gmail.com>
2020-05-11 23:01:19 +0300

    added router and some components

--- src/App.js
+++ src/App.js
@@ -4,15 +4,27 @@ import './App.css';
 import NewsList from './components/news-list/news-list'
 import Footer from "./components/footer/footer";
 import Header from "./components/header/header";
+import LeagueLeaders from "./components/leagueLeaders/leagueLeaders";
+import Contact from "./components/contact/contact";
+import About from "./components/about/about";
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 function App() {
-  return (
-    <div>
-      <Header></Header>
-      <NewsList></NewsList>
-      <Footer/>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Header></Header>
+                <div className="main-container">
+                    <Switch>
+                        <Route path='/leaders' component={ LeagueLeaders } />
+                        <Route path='/contact' component={ Contact } />
+                        <Route path='/about' component= { About } />
+                        <Route path='/' component={ NewsList } />
+                    </Switch>
+                </div>
+            <Footer/>
+        </BrowserRouter>
+    );
 }
 
 export default App;
